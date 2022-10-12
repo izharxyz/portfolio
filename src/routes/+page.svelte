694bc8adec114b5607862aeff94d8d9eea3d3506
zzedddd <melegend.forever@protonmail.com>
2022-10-12 23:38:25 +0530

    added a little animations

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,18 +1,26 @@
 <script>
+    import { onMount } from "svelte";
     import Header from "$lib/portfolio/header.svelte";
     import Hero from "$lib/portfolio/hero.svelte";
     import About from "$lib/portfolio/about.svelte";
     import Works from "$lib/portfolio/works.svelte";
     import Contact from "$lib/portfolio/contact.svelte";
     import "../app.css";
+
+    let mounted = false;
+    onMount(() => mounted = true);
 </script>
+
 <title>ZED.CODES</title>
+
 <div class="text-white font-inter pb-12">
-    <Header/>
-    <Hero/>
-    <About/>
-    <Works/>
-    <Contact/>
+    {#if mounted}
+        <Header/>
+        <Hero/>
+        <About/>
+        <Works/>
+        <Contact/>
+    {/if}
 </div>
 
 
