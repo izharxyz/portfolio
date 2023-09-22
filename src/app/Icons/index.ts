f571f2f8147afe153a3ab0e34d597b70320844e7
zzedddd <melegend.forever@protonmail.com>
2023-09-23 01:36:28 +0530

    move svg icons to it's own component

--- src/app/Icons/index.ts
+++ src/app/Icons/index.ts
@@ -1,8 +1,10 @@
 export { default as Logo } from "./Logo";
-export { default as HeartIcon } from "./HeartIcon";
+
+export { default as AsteriskIcon } from "./AsteriskIcon";
 export { default as GithubIcon } from "./GithubIcon";
-export { default as LinkedinIcon } from "./LinkedinIcon";
-export { default as TwitterIcon } from "./TwitterIcon";
+export { default as HeartIcon } from "./HeartIcon";
 export { default as LeetcodeIcon } from "./LeetcodeIcon";
+export { default as LinkedinIcon } from "./LinkedinIcon";
 export { default as SparkMultiColorIcon } from "./SparkMultiColorIcon";
 export { default as StarsIcon } from "./StarsIcon";
+export { default as TwitterIcon } from "./TwitterIcon";
--- src/app/components/BuildSomething.tsx
+++ src/app/components/BuildSomething.tsx
@@ -1,34 +1,14 @@
+import { AsteriskIcon } from "@/app/Icons";
+
 export default function BuildSomething() {
     return (
         <div className="w-full">
             <div className="bg-rose-300 text-violet-200 w-full mt-5 md:mt-10 lg:mt-20 h-[32rem] overflow-hidden relative">
                 <div className="absolute -left-40 top-24 text-teal-400">
-                    <svg
-                        className="w-80 h-80"
-                        xmlns="http://www.w3.org/2000/svg"
-                        width="16"
-                        height="16"
-                        viewBox="0 0 16 16"
-                    >
-                        <path
-                            fill="currentColor"
-                            d="m15.9 5.7l-2-3.4L10 4.5V0H6v4.5L2 2.3L0 5.7L3.9 8L0 10.3l2 3.4l4-2.2V16h4v-4.5l3.9 2.2l2-3.4l-4-2.3z"
-                        />
-                    </svg>
+                    <AsteriskIcon className="w-80 h-80" />
                 </div>
                 <div className="absolute -right-40 top-24 text-teal-400">
-                    <svg
-                        className="w-80 h-80"
-                        xmlns="http://www.w3.org/2000/svg"
-                        width="16"
-                        height="16"
-                        viewBox="0 0 16 16"
-                    >
-                        <path
-                            fill="currentColor"
-                            d="m15.9 5.7l-2-3.4L10 4.5V0H6v4.5L2 2.3L0 5.7L3.9 8L0 10.3l2 3.4l4-2.2V16h4v-4.5l3.9 2.2l2-3.4l-4-2.3z"
-                        />
-                    </svg>
+                    <AsteriskIcon className="w-80 h-80" />
                 </div>
                 <div
                     className="absolute inset-0 text-black flex flex-col gap-10 md:gap-20 items-center justify-center p-10 md:p-20"
