5de4a760c4d86decb533b2404e3478e865e73e2e
zzedddd <melegend.forever@protonmail.com>
2023-09-23 01:30:27 +0530

    move svg icons to it's own component

--- src/app/Icons/index.ts
+++ src/app/Icons/index.ts
@@ -1,3 +1,8 @@
 export { default as Logo } from "./Logo";
 export { default as HeartIcon } from "./HeartIcon";
 export { default as GithubIcon } from "./GithubIcon";
+export { default as LinkedinIcon } from "./LinkedinIcon";
+export { default as TwitterIcon } from "./TwitterIcon";
+export { default as LeetcodeIcon } from "./LeetcodeIcon";
+export { default as SparkMultiColorIcon } from "./SparkMultiColorIcon";
+export { default as StarsIcon } from "./StarsIcon";
