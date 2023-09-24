ca03904fec42bc6fb2824d0d79a8b98736b7c4d4
zzedddd <melegend.forever@protonmail.com>
2023-09-24 22:58:25 +0530

    add smooth scroll

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -43,7 +43,7 @@ export default function RootLayout({
     children: React.ReactNode;
 }) {
     return (
-        <html lang="en">
+        <html lang="en" className="scroll-smooth">
             <body
                 className={`${clashDisplay.className} font-clash overflow-x-hidden`}
             >
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,7 +13,7 @@ import {
 
 export default function Home() {
     return (
-        <main className="flex min-h-screen items-center justify-center sm:p-5 md:p-10 lg:p-20 scroll-smooth duration-300">
+        <main className="flex min-h-screen items-center justify-center sm:p-5 md:p-10 lg:p-20 duration-300">
             <div className="flex flex-col min-h-screen w-full items-center justify-center bg-violet-200 border-2 border-black overflow-hidden">
                 <Header />
                 <Hero />
