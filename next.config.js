6d67780af975f9b9a72b68235524c9f0e4e485ac
zzedddd <melegend.forever@protonmail.com>
2023-09-27 04:05:35 +0530

    remove experimental server actions

--- next.config.js
+++ next.config.js
@@ -1,8 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    experimental: {
-        serverActions: true,
-    },
     eslint: {
         // Warning: This allows production builds to successfully complete even if
         // your project has ESLint errors.
