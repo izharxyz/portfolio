e647878bfd44fa0e88081355602f43b3c11a7594
zzedddd <melegend.forever@protonmail.com>
2023-10-22 00:49:14 -0530

    Init portfolio v3.0

---
+++ src/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+    return (
+        <div className="min-h-screen grid grid-cols-1 md:grid-cols-2 items-center">
+            <h1 className="text-8xl tracking-tighter">Mohamed Izhar</h1>
+        </div>
+    );
+}
