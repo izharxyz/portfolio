ee972826fb931215cac5188a492e73cc6d20378b
0neTrueG0D <mohamed@izhar.xyz>
2023-12-17 01:49:20 +0530

    NUKE everything and startover

--- src/app/components/index.ts
+++
@@ -1,4 +0,0 @@
-export { default as Hero } from "./Hero";
-export { default as Header } from "./Header";
-export { default as About } from "./About";
-export { default as TechStack } from "./TechStack";
