8fb8c38e706235caba1569996d94e8af400c88ee
0neTrueG0D <mohamed@izhar.xyz>
2024-03-20 04:25:35 +0530

    chore: Remove unnecessary welcome message in BeforeLogin component

--- src/payload/components/BeforeLogin/index.tsx
+++ src/payload/components/BeforeLogin/index.tsx
@@ -5,7 +5,6 @@ const BeforeLogin: React.FC = () => {
     <div>
       <p>
         <b>Welcome to your dashboard!</b>
-        {' This is where site admins will log in to manage your website.'}
       </p>
     </div>
   )
