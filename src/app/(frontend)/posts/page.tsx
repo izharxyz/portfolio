a2c0b37186a3d932192fafdf4e5852bddfa89681
0neTrueG0D <mohamed@izhar.xyz>
2024-03-28 02:35:35 +0530

    chore: Update page title in posts/page.tsx

--- src/app/(frontend)/posts/page.tsx
+++ src/app/(frontend)/posts/page.tsx
@@ -23,7 +23,7 @@ export default async function Page() {
     <div className="pt-24 pb-24">
       <div className="container mb-16">
         <div className="prose dark:prose-invert max-w-none">
-          <h1>Posts</h1>
+          <h1>All Posts</h1>
         </div>
       </div>
 
@@ -47,6 +47,6 @@ export default async function Page() {
 
 export function generateMetadata(): Metadata {
   return {
-    title: `Payload Website Template Posts`,
+    title: `izh | All Posts`,
   }
 }
