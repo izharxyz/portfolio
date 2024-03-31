d80e61ce28f7815e4d61237ea3f21e0c1b138df4
0neTrueG0D <mohamed@izhar.xyz>
2024-04-01 02:39:35 +0530

    chore: Update Logo component styling in Logo.tsx

--- src/app/components/Logo/Logo.tsx
+++ src/app/components/Logo/Logo.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export const Logo = () => {
   return (
-    <div className="text-3xl font-extrabold tracking-wider bg-gradient-to-br from-cyan-500 to-indigo-500 bg-clip-text text-transparent">
+    <div className="text-2xl md:text-3xl font-extrabold tracking-wider bg-gradient-to-r from-purple-500 to-blue-500 bg-clip-text text-transparent">
       {'<izh />'}
     </div>
   )
