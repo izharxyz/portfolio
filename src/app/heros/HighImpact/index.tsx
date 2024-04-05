640b1fdd768f5d614c5b8a018d0f340acba13121
0neTrueG0D <mohamed@izhar.xyz>
2024-04-06 01:19:39 +0530

    feat: redesign Hero with animation, sparkle background and typerwriter effects (all from aceternity ui)

--- src/app/heros/HighImpact/index.tsx
+++ src/app/heros/HighImpact/index.tsx
@@ -5,8 +5,9 @@ import React, { useEffect } from 'react'
 import type { Page } from '../../../payload-types'
 
 import { CMSLink } from '../../components/Link'
-import { Media } from '../../components/Media'
-import RichText from '../../components/RichText'
+
+import { SparklesCore } from '@/components/ui/sparkles'
+import { TypewriterEffect } from '@/components/ui/typewriter-effect'
 
 export const HighImpactHero: React.FC<Page['hero']> = ({ links, media, richText }) => {
   const { setHeaderTheme } = useHeaderTheme()
@@ -15,30 +16,65 @@ export const HighImpactHero: React.FC<Page['hero']> = ({ links, media, richText 
     setHeaderTheme('dark')
   })
 
+  const words = [
+    {
+      text: 'I',
+      className: 'text-5xl md:text-6xl lg:text-7xl font-bold z-20',
+    },
+    {
+      text: 'create',
+      className: 'text-5xl md:text-6xl lg:text-7xl font-bold z-20',
+    },
+    {
+      text: 'digital',
+      className: 'text-5xl md:text-6xl lg:text-7xl font-bold z-20',
+    },
+    {
+      text: 'experiences',
+      className: 'text-5xl md:text-6xl lg:text-7xl font-bold text-purple-500 z-20',
+    },
+    {
+      text: 'that',
+      className: 'text-5xl md:text-6xl lg:text-7xl font-bold z-20',
+    },
+    {
+      text: 'inspire',
+      className: 'text-5xl md:text-6xl lg:text-7xl font-bold text-blue-500 z-20',
+    },
+  ]
+
   return (
-    <div className="relative -mt-[10.4rem] flex items-end text-white" data-theme="dark">
-      <div className="container mb-32 z-10 relative">
-        <div className="max-w-[34rem]">
-          <RichText className="mb-6" content={richText} enableGutter={false} />
-          {Array.isArray(links) && links.length > 0 && (
-            <ul className="flex gap-4">
-              {links.map(({ link }, i) => {
-                return (
-                  <li key={i}>
-                    <CMSLink {...link} />
-                  </li>
-                )
-              })}
-            </ul>
-          )}
-        </div>
+    <div className="h-screen w-full -mt-[10.4rem] relative flex flex-col items-center justify-center overflow-hidden">
+      <div className="w-full absolute inset-0 h-screen">
+        <SparklesCore
+          id="tsparticlesfullpage"
+          background="transparent"
+          minSize={0.6}
+          maxSize={1.5}
+          particleDensity={50}
+          className="w-full h-full"
+          particleColor="#FFFFFF"
+        />
       </div>
-      <div className="min-h-screen select-none">
-        {typeof media === 'object' && (
-          <React.Fragment>
-            <Media fill imgClassName="-z-10 object-cover" priority resource={media} />
-            <div className="absolute pointer-events-none left-0 bottom-0 w-full h-1/2 bg-gradient-to-t from-black to-transparent" />
-          </React.Fragment>
+      <div className="text-center relative z-20 flex flex-col items-center justify-center">
+        <p className="mb-6 text-lg md:text-xl lg:text-2xl text-muted-foreground">
+          Hello, my name is{' '}
+          <span className="font-bold text-secondary-foreground">Mohamed Izhar</span>
+        </p>
+        <div className="max-w-6xl mb-16">
+          <TypewriterEffect words={words} />
+        </div>
+
+        {Array.isArray(links) && links.length > 0 && (
+          <ul className="flex gap-4">
+            {links.map(({ link }, i) => {
+              return (
+                <li key={i}>
+                  <CMSLink {...link} />
+                </li>
+              )
+            })}
+          </ul>
         )}
       </div>
     </div>
