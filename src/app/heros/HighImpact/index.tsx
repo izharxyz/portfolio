b9902c3323c053ce3714b6ea545ebf1c07f87cc9
0neTrueG0D <mohamed@izhar.xyz>
2024-04-08 01:49:39 +0530

    refactor: Update HighImpactHero component styling and add padding to improve layout

--- src/app/heros/HighImpact/index.tsx
+++ src/app/heros/HighImpact/index.tsx
@@ -31,7 +31,7 @@ export const HighImpactHero: React.FC<Page['hero']> = ({ links, media, richText 
     },
     {
       text: 'experiences',
-      className: 'text-5xl md:text-6xl lg:text-7xl font-bold text-purple-500 z-20',
+      className: 'text-5xl md:text-6xl lg:text-7xl font-bold text-purple-600 z-20',
     },
     {
       text: 'that',
@@ -39,12 +39,12 @@ export const HighImpactHero: React.FC<Page['hero']> = ({ links, media, richText 
     },
     {
       text: 'inspire',
-      className: 'text-5xl md:text-6xl lg:text-7xl font-bold text-blue-500 z-20',
+      className: 'text-5xl md:text-6xl lg:text-7xl font-bold text-purple-600 z-20',
     },
   ]
 
   return (
-    <div className="h-screen w-full -mt-[10.4rem] relative flex flex-col items-center justify-center overflow-hidden">
+    <div className="h-screen w-full -mt-[10.4rem] px-4 relative flex flex-col items-center justify-center overflow-hidden">
       <div className="w-full absolute inset-0 h-screen">
         <SparklesCore
           id="tsparticlesfullpage"
@@ -58,15 +58,14 @@ export const HighImpactHero: React.FC<Page['hero']> = ({ links, media, richText 
       </div>
       <div className="text-center relative z-20 flex flex-col items-center justify-center">
         <p className="mb-6 text-lg md:text-xl lg:text-2xl text-muted-foreground">
-          Hello, my name is{' '}
-          <span className="font-bold text-secondary-foreground">Mohamed Izhar</span>
+          Hello, my name is <span className="text-secondary-foreground">Mohamed Izhar</span>
         </p>
         <div className="max-w-6xl mb-16">
           <TypewriterEffect words={words} />
         </div>
 
         {Array.isArray(links) && links.length > 0 && (
-          <ul className="flex gap-4">
+          <ul className="flex flex-col md:flex-row gap-4">
             {links.map(({ link }, i) => {
               return (
                 <li key={i}>
