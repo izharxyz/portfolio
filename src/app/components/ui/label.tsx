cde594614dfc90f19442f0ce8c53595088b44ae9
0neTrueG0D <mohamed@izhar.xyz>
2024-04-15 02:14:39 +0530

    refactor: Update Label component to use consistent styling and remove unused code

--- src/app/components/ui/label.tsx
+++ src/app/components/ui/label.tsx
@@ -1,19 +1,21 @@
 'use client'
-
-import { cn } from '@/utilities/cn'
-import * as LabelPrimitive from '@radix-ui/react-label'
-import { type VariantProps, cva } from 'class-variance-authority'
 import * as React from 'react'
+import * as LabelPrimitive from '@radix-ui/react-label'
 
-const labelVariants = cva(
-  'text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70',
-)
+import { cn } from '@/lib/utils'
 
 const Label = React.forwardRef<
-  React.ElementRef<typeof LabelPrimitive.Root>,
-  React.ComponentPropsWithoutRef<typeof LabelPrimitive.Root> & VariantProps<typeof labelVariants>
+  React.ComponentRef<typeof LabelPrimitive.Root>,
+  React.ComponentPropsWithoutRef<typeof LabelPrimitive.Root>
 >(({ className, ...props }, ref) => (
-  <LabelPrimitive.Root className={cn(labelVariants(), className)} ref={ref} {...props} />
+  <LabelPrimitive.Root
+    ref={ref}
+    className={cn(
+      'text-sm font-medium text-black dark:text-white leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70',
+      className,
+    )}
+    {...props}
+  />
 ))
 Label.displayName = LabelPrimitive.Root.displayName
 
