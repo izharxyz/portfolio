793062d20312396012e6554d7820afc625187135
0neTrueG0D <mohamed@izhar.xyz>
2024-04-20 01:59:39 +0530

    refactor: Update Input and Textarea components to use consistent background color and remove unused styles

--- src/app/components/ui/textarea.tsx
+++ src/app/components/ui/textarea.tsx
@@ -39,7 +39,7 @@ const Textarea = React.forwardRef<HTMLTextAreaElement, TextareaProps>(
       >
         <textarea
           className={cn(
-            `flex min-h[80px] w-full border-none bg-gray-50 dark:bg-zinc-800 text-black dark:text-white shadow-input rounded-md px-3 py-2 text-sm file:border-0 file:bg-transparent 
+            `flex min-h[80px] w-full border-none bg-background text-black dark:text-white shadow-input rounded-md px-3 py-2 text-sm file:border-0 file:bg-transparent 
             file:text-sm file:font-medium placeholder:text-neutral-400 dark:placeholder-text-neutral-600 
             focus-visible:outline-none focus-visible:ring-[2px] focus-visible:ring-neutral-400 dark:focus-visible:ring-neutral-600
             disabled:cursor-not-allowed disabled:opacity-50
