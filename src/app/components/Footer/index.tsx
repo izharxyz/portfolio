dd400d74b0daa8899d2c5c12e367c7ae421727d1
Mohamed Izhar <mohamed@izhar.xyz>
2024-05-13 02:03:22 +0000

    style: Update Footer component with new styles and link colors

--- src/app/components/Footer/index.tsx
+++ src/app/components/Footer/index.tsx
@@ -14,16 +14,16 @@ export async function Footer() {
   const navItems = footer?.navItems || []
 
   return (
-    <footer className="border-t border-border bg-black dark:bg-card text-white">
+    <footer className="border-t border-border bg-card text-muted-foreground">
       <div className="container py-8 gap-8 flex flex-col justify-center items-center md:flex-row md:justify-between">
-        <Link className="flex items-center" href="/">
+        <Link className="flex items-center text-foreground" href="/">
           <Logo />
         </Link>
 
         <div className="flex items-center flex-row gap-4 md:items-center">
           <nav className="flex flex-row gap-4">
             {navItems.map(({ link }, i) => {
-              return <CMSLink className="text-white" key={i} {...link} />
+              return <CMSLink className="text-muted-foreground" key={i} {...link} />
             })}
           </nav>
         </div>
