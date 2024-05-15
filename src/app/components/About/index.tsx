7c254261a1a464728c6baa60197382cf231ea9cb
Mohamed Izhar <mohamed@izhar.xyz>
2024-05-15 02:45:22 +0000

    style: Add reveal text effect and change bento layout

--- src/app/components/About/index.tsx
+++ src/app/components/About/index.tsx
@@ -1,6 +1,7 @@
 'use client'
 import React from 'react'
 import { Skills } from '../Skills'
+import { TextRevealEffect } from '../ui/textRevealEffect'
 
 export const About = () => {
   return (
@@ -11,11 +12,11 @@ export const About = () => {
           <p>The story of the beginning of the company</p>
         </div>
         <div className="w-full xl:w-2/5 flex flex-col lg:flex-row xl:flex-col gap-4">
-          <div className="w-full min-h-[12rem] lg:min-h-[16rem] rounded-xl p-4 border">
+          <div className="w-full min-h-[12rem] lg:grow rounded-xl p-4 border">
             <h3>Githb Globe</h3>
             <p>I&apos;m very flexible with time zone communication</p>
           </div>
-          <div className="w-full min-h-[12rem] lg:min-h-[16rem] rounded-xl p-4 border">
+          <div className="w-full min-h-[12rem] rounded-xl p-4 border">
             <h3>Socials</h3>
             <p>Catch up with me on social media</p>
           </div>
@@ -24,12 +25,14 @@ export const About = () => {
 
       <div className="flex gap-4 flex-col lg:grid lg:grid-cols-2 xl:flex xl:flex-row w-full mt-4">
         <div className="w-full xl:w-2/5 flex flex-col gap-4">
-          <div className="w-full min-h-[12rem] lg:min-h-[16rem] rounded-xl p-4 border">
-            <h3>Quote</h3>
-            <p>An enthusiastic with a passion for technology</p>
+          <div className="w-full min-h-[12rem] rounded-xl border flex items-center justify-center overflow-hidden">
+            <TextRevealEffect
+              text="An enthusiast with a passion for technology"
+              revealText="A gamer with knack for side quests"
+            />
           </div>
 
-          <div className="w-full min-h-[12rem] lg:min-h-[16rem] rounded-xl p-4 border">
+          <div className="w-full min-h-[12rem] lg:grow rounded-xl p-4 border">
             <h3>Copy my email</h3>
             <p>Let&apos;s create something beautiful</p>
           </div>
@@ -39,9 +42,9 @@ export const About = () => {
           aria-label="tech stack"
         >
           <div className="w-full flex items-center text-center">
-            <h4 className="w-full font-semibold text-lg md:text-xl lg:text-2xl">My Tech Stack</h4>
+            <h3 className="w-full font-bold text-3xl lg:text-4xl tracking-tight">My tech stack</h3>
           </div>
-          <div className="pt-4 lg:pt-10">
+          <div id="skills" className="pt-4 lg:pt-8">
             <Skills />
           </div>
         </div>
