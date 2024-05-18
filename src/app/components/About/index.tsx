f976b360a1880f657ca78218fc71a673e2020f4b
Mohamed Izhar <mohamed@izhar.xyz>
2024-05-19 01:25:22 +0530

    refactor: Revert socialIcon component to inline HTML and add freelancing profile icons with animation

--- src/app/components/About/index.tsx
+++ src/app/components/About/index.tsx
@@ -2,9 +2,14 @@
 import React from 'react'
 import Link from 'next/link'
 import { Skills } from './Skills'
-import { TextRevealEffect } from '../ui/textRevealEffect'
+import { TextRevealEffect } from '../ui/TextRevealEffect'
+import { FlipWords } from '../ui/FlipWords'
+import { GradientButton } from '../ui/GradientButton'
 
-import { IconType } from 'react-icons/lib'
+import { SiFreelancer } from 'react-icons/si'
+import { FaSquareUpwork } from 'react-icons/fa6'
+import { TbBrandFiverr } from 'react-icons/tb'
+
 import { IoLogoGithub } from 'react-icons/io'
 import { FaLinkedin } from 'react-icons/fa'
 import { FaXTwitter } from 'react-icons/fa6'
@@ -12,20 +17,69 @@ import { FaInstagram } from 'react-icons/fa6'
 
 export const About = () => {
   return (
-    <section id="about" className="container pt-24 pb-24">
+    <section
+      id="about"
+      className="container pt-24 pb-24 text-foreground dark:text-muted-foreground"
+    >
       <div className="flex gap-4 flex-col xl:flex-row w-full">
-        <div className="w-full xl:w-3/5 min-h-[24rem] lg:min-h-[32rem] rounded-xl p-4 border">
-          <h3>The Beginning</h3>
-        </div>
-        <div className="w-full xl:w-2/5 flex flex-col lg:flex-row xl:flex-col gap-4">
-          <div className="w-full min-h-[12rem] lg:grow rounded-xl p-4 border relative">
-            <h3>hire me on</h3>
+        <div className="w-full xl:w-3/5 min-h-[24rem] lg:min-h-[32rem] rounded-xl p-4 lg:p-8 border flex items-center justify-center">
+          <div className="w-full text-center">
+            <h3 className="font-bold text-3xl lg:text-4xl tracking-tight">
+              <FlipWords words={['Developer', 'Designer', 'Engineer', 'Enthusiast']} />
+            </h3>
+            <p className="text-lg md:text-xl lg:text-2xl text-muted-foreground font-normal mt-8 text-left">
+              I am a full-stack developer with a passion for creating beautiful and functional
+              applications. I am a self-taught developer with a knack for learning new technologies
+              and creating innovative solutions to complex problems.
+            </p>
           </div>
-          <div className="w-full min-h-[12rem] rounded-xl p-4 border flex gap-8 items-center justify-center group/social">
-            <SocialIcon href="#" color="foreground" rotate="rotate-12" Icon={IoLogoGithub} />
-            <SocialIcon href="#" color="blue-500" rotate="-rotate-12" Icon={FaLinkedin} />
-            <SocialIcon href="#" color="foreground" rotate="rotate-12" Icon={FaXTwitter} />
-            <SocialIcon href="#" color="rose-500" rotate="-rotate-12" Icon={FaInstagram} />
+        </div>
+        <div className="w-full xl:w-2/5 flex flex-col lg:grid lg:grid-cols-2 xl:flex xl:flex-col gap-4">
+          <div className="w-full min-h-[12rem] lg:grow rounded-xl p-4 md:p-8 border flex items-center justify-between group/hire">
+            <h3 className="font-bold text-3xl lg:text-4xl tracking-tight group-hover/hire:translate-x-2 duration-300">
+              Hire me on
+            </h3>
+            <div className="flex flex-col gap-4">
+              <Link href="#" className="group-hover/hire:rotate-12 duration-300">
+                <GradientButton>
+                  <div className="w-32 flex gap-4 justify-center items-center">
+                    <span>Freelancer</span>
+                    <SiFreelancer className="h-6 w-6" />
+                  </div>
+                </GradientButton>
+              </Link>
+              <Link href="#" className="group-hover/hire:-rotate-12 duration-300">
+                <GradientButton>
+                  <div className="w-32 flex gap-4 justify-center items-center">
+                    <span>Fiverr</span>
+                    <TbBrandFiverr className="h-6 w-6" />
+                  </div>
+                </GradientButton>
+              </Link>
+              <Link href="#" className="group-hover/hire:rotate-12 duration-300">
+                <GradientButton>
+                  <div className="w-32 flex gap-4 justify-center items-center">
+                    <span>Upwork</span>
+                    <FaSquareUpwork className="h-6 w-6" />
+                  </div>
+                </GradientButton>
+              </Link>
+            </div>
+          </div>
+          <div className="w-full min-h-[12rem] rounded-xl p-4 lg:p-8 border flex gap-8 items-center justify-center group/social">
+            <Link href="#" className="h-10 w-10 md:w-12 md:h-12">
+              <IoLogoGithub className="h-10 w-10 md:w-12 md:h-12 text-muted-foreground group-hover/social:rotate-12 hover:scale-125 group-hover/social:text-foreground duration-300" />
+            </Link>
+            <Link href="#" className="h-10 w-10 md:w-12 md:h-12">
+              <FaLinkedin className="h-10 w-10 md:w-12 md:h-12 text-muted-foreground group-hover/social:-rotate-12 hover:scale-125 group-hover/social:text-blue-500 duration-300" />
+            </Link>
+
+            <Link href="#" className="h-10 w-10 md:w-12 md:h-12">
+              <FaXTwitter className="h-10 w-10 md:w-12 md:h-12 text-muted-foreground group-hover/social:rotate-12 hover:scale-125 group-hover/social:text-foreground duration-300" />
+            </Link>
+            <Link href="#" className="h-10 w-10 md:w-12 md:h-12">
+              <FaInstagram className="h-10 w-10 md:w-12 md:h-12 text-muted-foreground group-hover/social:-rotate-12 hover:scale-125 group-hover/social:text-rose-500 duration-300" />
+            </Link>
           </div>
         </div>
       </div>
@@ -39,17 +93,19 @@ export const About = () => {
             />
           </div>
 
-          <div className="w-full min-h-[12rem] lg:grow rounded-xl p-4 border">
+          <div className="w-full min-h-[12rem] lg:grow rounded-xl p-4 lg:p-8 border">
             <h3>Copy my email</h3>
             <p>Let&apos;s create something beautiful</p>
           </div>
         </div>
         <div
-          className="w-full xl:w-3/5 min-h-[24rem] lg:min-h-[32rem] rounded-xl p-4 lg:p-10 border flex flex-col justify-between"
+          className="w-full xl:w-3/5 min-h-[24rem] lg:min-h-[32rem] rounded-xl p-4 lg:p-8 border flex flex-col justify-between group/tech"
           aria-label="tech stack"
         >
           <div className="w-full flex items-center text-center">
-            <h3 className="w-full font-bold text-3xl lg:text-4xl tracking-tight">My tech stack</h3>
+            <h3 className="w-full font-bold text-3xl lg:text-4xl tracking-tight group-hover/tech:translate-x-2 duration-300">
+              My tech stack
+            </h3>
           </div>
           <div id="skills" className="pt-4 lg:pt-8 pb-2 pr-2">
             <Skills />
@@ -57,25 +113,5 @@ export const About = () => {
         </div>
       </div>
     </section>
-  )
-}
-
-const SocialIcon = ({
-  href,
-  color,
-  rotate,
-  Icon,
-}: {
-  href: string
-  color: string
-  rotate: string
-  Icon: IconType
-}) => {
-  return (
-    <Link href={href} className="h-10 w-10 md:w-12 md:h-12">
-      <Icon
-        className={`h-10 w-10 md:w-12 md:h-12 text-muted-foreground group-hover/social:${rotate} hover:scale-125 group-hover/social:text-${color} duration-300`}
-      />
-    </Link>
   )
 }
