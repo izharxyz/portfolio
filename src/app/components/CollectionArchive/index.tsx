8a955eccfb9fdee28aa1a8a9b538f764acb50c4c
Mohamed Izhar <mohamed@izhar.xyz>
2024-05-21 01:25:22 +0530

    style: Update grid layout in CollectionArchive component

--- src/app/components/CollectionArchive/index.tsx
+++ src/app/components/CollectionArchive/index.tsx
@@ -15,11 +15,11 @@ export const CollectionArchive: React.FC<Props> = (props) => {
   return (
     <div className={cn('container')}>
       <div>
-        <div className="grid grid-cols-4 sm:grid-cols-8 lg:grid-cols-12 gap-y-4 gap-x-4 lg:gap-y-8 lg:gap-x-8 xl:gap-x-8">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-x-4 md:gap-x-8">
           {posts?.map((result, index) => {
             if (typeof result === 'object' && result !== null) {
               return (
-                <div className="col-span-4" key={index}>
+                <div className="" key={index}>
                   <Card className="h-full" doc={result} relationTo="posts" showCategories />
                 </div>
               )
