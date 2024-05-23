715caf921fac023dcf642c8026e9e632da99ba2c
Mohamed Izhar <mohamed@izhar.xyz>
2024-05-24 00:59:22 +0530

    chore: Remove unnecessary code in PageRange component

--- src/app/components/PageRange/index.tsx
+++ src/app/components/PageRange/index.tsx
@@ -44,11 +44,6 @@ export const PageRange: React.FC<{
   return (
     <div className={[className, 'font-semibold'].filter(Boolean).join(' ')}>
       {(typeof totalDocs === 'undefined' || totalDocs === 0) && 'Search produced no results.'}
-      {typeof totalDocs !== 'undefined' &&
-        totalDocs > 0 &&
-        `Showing ${indexStart}${indexStart > 0 ? ` - ${indexEnd}` : ''} of ${totalDocs} ${
-          totalDocs > 1 ? plural : singular
-        }`}
     </div>
   )
 }
