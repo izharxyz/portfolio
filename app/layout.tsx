3bb02648bd586be99f84544c7f18423fb01fb1ff
Mohamed Izhar <mohamed@izhar.xyz>
2024-06-03 01:59:29 +0530

    style: add scroll smooth

--- app/layout.tsx
+++ app/layout.tsx
@@ -64,7 +64,7 @@ export default function RootLayout({
     children: React.ReactNode;
 }>) {
     return (
-        <html lang="en">
+        <html lang="en" className="scroll-smooth">
             <body
                 className={`${geistSans.variable} ${geistMono.variable} antialiased overflow-x-hidden`}
             >
