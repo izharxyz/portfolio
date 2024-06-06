31692cbd480ce7af1ee954f057b92f634c19d6fb
Mohamed Izhar <mohamed@izhar.xyz>
2024-06-07 04:11:43 +0530

    feat: add initial Blog component with placeholder message

---
+++ app/blog/page.tsx
@@ -0,0 +1,7 @@
+export default function Blog() {
+    return (
+        <main className="w-screen min-h-screen flex items-center justify-center">
+            <p className="text-muted-foreground">Blog coming soon...</p>
+        </main>
+    );
+}
