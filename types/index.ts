872968c2b16014ea9a99de8a6de10436dbc2cb5e
Mohamed Izhar <mohamed@izhar.xyz>
2024-06-08 01:31:43 +0530

    feat: add project interface to define project structure in types/index.ts

---
+++ types/index.ts
@@ -0,0 +1,12 @@
+export interface project {
+    id: number;
+    title: string;
+    description: string;
+    category: string;
+    image: string;
+    source_code_url: string;
+    site_url: string;
+    detail: string;
+    created_at: string;
+    updated_at: string;
+}
