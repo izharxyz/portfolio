65080cfaa06f5faa90cbe0ea6148e850138074aa
Mohamed Izhar <mohamed@izhar.xyz>
2024-06-17 01:45:43 +0530

    feat: update LatestPosts component to fetch and display up to 12 recent blog posts

--- app/blog/LatestPosts.tsx
+++ app/blog/LatestPosts.tsx
@@ -1,34 +1,36 @@
-import Link from "next/link";
-import { PrimaryButton } from "@/components/ui/Button";
 import Card from "@/components/ui/PostCard";
+import { PostResponse } from "@/types";
 
-export default function LatestPosts() {
+export default async function LatestPosts() {
+    const featuredResponse = await fetch(
+        `${process.env.NEXT_PUBLIC_API_URL}/blog/posts/`,
+        { cache: "force-cache" }
+    );
+
+    const posts: PostResponse = await featuredResponse.json();
+
+    // If there are fewer than 12 posts, repeat the posts until we have exactly 12
+    if (posts.results.length < 12) {
+        const repeatedPosts = [];
+        while (repeatedPosts.length < 12) {
+            repeatedPosts.push(...posts.results.reverse());
+        }
+        // Slice the array to get exactly 12 posts
+        posts.results = repeatedPosts.slice(0, 12);
+    }
+
     return (
         <>
-            <div className="flex items-center justify-between">
-                <h3 className="text-2xl md:text-3xl text-accent font-semibold">
-                    Latest Posts
-                </h3>
-                <Link href="/blog">
-                    <PrimaryButton>View all posts</PrimaryButton>
-                </Link>
-            </div>
+            <h3 className="text-2xl md:text-3xl text-accent font-semibold">
+                Latest Posts
+            </h3>
+
             <p className="text-muted-foreground text-sm md:text-base mt-2 max-w-xs md:max-w-lg">
                 Here are some of the latest posts from the blog. Enjoy reading!
             </p>
             <div className="mt-10 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5">
-                {Array.from({ length: 6 }).map((_, index) => (
-                    <Card
-                        key={index}
-                        post={{
-                            title: "An awesome story of a web developer",
-                            description:
-                                "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-                            image: "/images/web4.jpeg",
-                            categories: ["Web Development", "React", "Next.js"],
-                            slug: "post-slug",
-                        }}
-                    />
+                {posts.results.map((post, index) => (
+                    <Card key={index} post={post} />
                 ))}
             </div>
         </>
